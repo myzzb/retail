fe51010e9817fbd82c54eea471a4ce22453f457d
myzzb <17691228632@163.com>
2021-07-04 22:27:02 +0800

    Initial commit

---
+++ retail-admin/src/test/java/com/myzzb/retail/admin/RetailAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myzzb.retail.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
