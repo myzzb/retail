8a57796038fbf8d7cf0a1d5b5d5112f8b537fb54
myzzb <17691228632@163.com>
2021-07-07 09:14:48 +0800

    Feat:初始化readme文件

---
+++ retail-common/src/main/java/com/myzzb/retail/common/RetailCommonApplication.java
@@ -0,0 +1,13 @@
+package com.myzzb.retail.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailCommonApplication.class, args);
+    }
+
+}
