ad61262fa9fede701663c15e470f74c6414afa9a
myzzb <17691228632@163.com>
2021-07-08 09:17:20 +0800

    Feat:初始化readme文件

---
+++ retail-mbg/src/main/java/com/myzzb/retail/mbg/RetailMbgApplication.java
@@ -0,0 +1,13 @@
+package com.myzzb.retail.mbg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailMbgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetailMbgApplication.class, args);
+    }
+
+}
